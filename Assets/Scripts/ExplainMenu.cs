a8d69fcf9f1097658a9548394034168f0fd4aa9f
Aron Dexter <AB5820@student.jamk.fi>
2024-01-26 21:40:10 +0200

    added main menu

---
+++ Assets/Scripts/ExplainMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExplainMenu : MonoBehaviour
+{
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("Bruh");
+    }
+}
